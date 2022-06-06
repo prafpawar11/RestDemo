04961ab775fddf9b3711ff58018a32555cfec913
prafpawar11 <prafpawar11@gmail.com>
2022-06-06 20:41:43 +0530

    aaa

--- src/test/java/Demo/Demo.java
+++ src/test/java/Demo/Demo.java
@@ -4,7 +4,6 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Demo");
-		System.out.println("Demo2");
-		System.out.println("Dem");
+	System.out.println(":Demo");
 	}
 }
